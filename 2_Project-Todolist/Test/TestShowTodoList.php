a38d97ed414fb7fbd2eb576abe3aea54e60c74c1
AriSyafri <diamondThink15@gmail.com>
2024-01-14 21:39:46 +0700

    membuat test menampilkan data

---
+++ 2_Project-Todolist/Test/TestShowTodoList.php
@@ -0,0 +1,9 @@
+<?php 
+    require_once "../Model/TodoList.php";
+    require_once "../BusinessLogic/ShowTodoList.php";
+
+    $todoList[1] = "belajar PHP Dasar";
+    $todoList[2] = "belajar PHP OOP";
+    $todoList[3] = "belajar PHP Database";
+
+    showTodoList();
