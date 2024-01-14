1b1c8f9947c66733028e7c5c7cd36319db18a75a
AriSyafri <diamondThink15@gmail.com>
2024-01-14 21:57:36 +0700

    var global $todoList ada 2 sehingga dihapus 1

--- 2_Project-Todolist/BusinessLogic/AddTodoList.php
+++ 2_Project-Todolist/BusinessLogic/AddTodoList.php
@@ -4,8 +4,6 @@
 function addTodoList(string $todo){
     global $todoList;
 
-    global $todoList;
-
     // ini agar nanti ketika jumlah data ditambah 1 ke indeks
     $number = sizeof($todoList) + 1;
 
