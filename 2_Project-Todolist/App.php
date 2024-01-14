817d1c146c39218de84a1ba2495a1310deb9b999
AriSyafri <diamondThink15@gmail.com>
2024-01-14 19:17:19 +0700

    memanggil business Logic tambah, menampilkan, dan menghapus

--- 2_Project-Todolist/App.php
+++ 2_Project-Todolist/App.php
@@ -1,5 +1,8 @@
 <?php
     require_once "Model/TodoList.php";
+    require_once "BussinesLogic/AddTodoList.php";
+    require_once "BussinesLogic/RemoveTodoList.php";
+    require_once "BussinesLogic/ShowTodoList.php";
 
     echo "Aplikasi to do list" . PHP_EOL;
 
