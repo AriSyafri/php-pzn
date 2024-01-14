9a1483bf0ad608531e48e6e9697287eed757a0a9
AriSyafri <diamondThink15@gmail.com>
2024-01-14 21:29:14 +0700

    menambah perulangan pada fungsi showTodoList

--- 2_Project-Todolist/BusinessLogic/ShowTodoList.php
+++ 2_Project-Todolist/BusinessLogic/ShowTodoList.php
@@ -3,4 +3,12 @@
 // menampilkan todo di list
 function showTodoList(){
 
+    global $todoList; // diambil dari model array TodoList.php
+
+    echo "TODOLIST" . PHP_EOL;
+
+    foreach ($todoList as $number => $value) {
+        echo "$number . $value" . PHP_EOL; 
+    }
+
 }
