fd224796f7c356e954cf1875ac8f198bc53d3906
AriSyafri <diamondThink15@gmail.com>
2024-01-14 21:45:22 +0700

    menambah isi fungsi  bisnis logic

--- 2_Project-Todolist/BusinessLogic/AddTodoList.php
+++ 2_Project-Todolist/BusinessLogic/AddTodoList.php
@@ -1,6 +1,14 @@
 <?php
 
 // menambah todo ke list
-function addTodoList(){
+function addTodoList(string $todo){
+    global $todoList;
+
+    global $todoList;
+
+    // ini agar nanti ketika jumlah data ditambah 1 ke indeks
+    $number = sizeof($todoList) + 1;
+
+    $todoList[$number] = $todo;
     
 }
