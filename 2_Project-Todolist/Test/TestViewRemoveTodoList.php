f5a6b50afa1625fb1cba7e39cc5bc14672bdd48a
AriSyafri <diamondThink15@gmail.com>
2024-01-15 20:12:27 +0700

    pengujian hapus todo list

---
+++ 2_Project-Todolist/Test/TestViewRemoveTodoList.php
@@ -0,0 +1,22 @@
+<?php 
+
+    require_once "../Model/TodoList.php";
+    require_once "../View/ViewRemoveTodoList.php";
+    require_once "../BusinessLogic/AddTodoList.php";
+    require_once "../BusinessLogic/ShowTodoList.php";
+
+    addTodoList("Ari");
+    addTodoList("ujang");
+    addTodoList("asep");
+    addTodoList("Ari");
+
+    showTodoList();
+    viewRemoveTodoList();
+
+    showTodoList();
+    viewRemoveTodoList();
+
+    showTodoList();
+    viewRemoveTodoList();
+
+    showTodoList();
