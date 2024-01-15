c4b971742a0172d15dab7344614539ba63d0bcac
AriSyafri <diamondThink15@gmail.com>
2024-01-15 20:11:31 +0700

    menambah fungsi view tambah data

--- 2_Project-Todolist/View/ViewAddTodoList.php
+++ 2_Project-Todolist/View/ViewAddTodoList.php
@@ -1,5 +1,19 @@
 <?php 
+require_once "../Model/TodoList.php";
+require_once "../Helper/Input.php";
+require_once "../BusinessLogic/AddTodoList.php";
+
 
 function viewAddTodoList(){
+    echo "Menambah Todo" . PHP_EOL;
+
+    $todo = input("Todo (x untuk batal)");
+
+    if($todo == "x") {
+        echo "Batal menambah todo" . PHP_EOL;
+    } else {
+        addTodoList($todo);
+    }
+
     
 }
