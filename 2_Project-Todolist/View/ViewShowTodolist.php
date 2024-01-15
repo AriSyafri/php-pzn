6e711cb93229cbd5a4872bc65d967509bb8942de
AriSyafri <diamondThink15@gmail.com>
2024-01-15 14:39:12 +0700

    menambah fungsi view menampilkan data dan menampilkan awalan program penampil data

--- 2_Project-Todolist/View/ViewShowTodolist.php
+++ 2_Project-Todolist/View/ViewShowTodolist.php
@@ -1,5 +1,37 @@
 <?php
 
+require_once "../BusinessLogic/ShowTodoList.php";
+require_once "../BusinessLogic/AddTodoList.php";
+require_once "../BusinessLogic/RemoveTodoList";
+require_once "../Helper/Input.php";
+
+
 function viewShowTodoList(){
+
+
+    while (true) {
+        showTodoList();
+
+        echo "Menu" . PHP_EOL;
+        echo "1. Tambah Todo" . PHP_EOL;
+        echo "2. Hapus Todo" . PHP_EOL;
+        echo "x. Keluar" . PHP_EOL;
     
+        $pilihan = input("Pilih");
+    
+        if($pilihan == "1") {
+            viewAddTodoList();
+        } else if($pilihan == "2") {
+            viewRemoveTodoList();
+        } else if($pilihan == "x") {
+            // keluar
+            break;
+        } else {
+            echo "Pilihan tidak di mengerti" . PHP_EOL;
+        }
+    }
+
+    echo "sampai jumpa lagi" . PHP_EOL;
+
+
 }
