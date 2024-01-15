12211b64409090cbdc78d6c87acc85a975239885
AriSyafri <diamondThink15@gmail.com>
2024-01-15 14:53:27 +0700

    menambah tes view untuk menampilkan data

---
+++ 2_Project-Todolist/Test/TestViewShowTodoList.php
@@ -0,0 +1,13 @@
+<?php 
+    require_once "../View/ViewShowTodoList.php";
+    require_once "../BusinessLogic/AddTodoList.php";
+    
+    addTodoList("tes");
+    addTodoList("ari");
+    addTodoList("syafri");
+    addTodoList("tes aja");
+
+
+    viewShowTodoList();
+
+
