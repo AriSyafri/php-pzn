f933958bd1889884094c75cb264cf053e68cc37f
AriSyafri <diamondThink15@gmail.com>
2024-01-19 22:10:04 +0700

    menambah repo untuk implementasi interface dimulai  menampilkan data

--- 5_TodoListOOP/Repository/TodolistRepository.php
+++ 5_TodoListOOP/Repository/TodolistRepository.php
@@ -9,6 +9,25 @@
 
             function remove(int $number): bool;
 
-            function findll():array;
+            function findAll(): array;
+        }
+
+        class TodolistRepositoryImpl implements TodolistRepository {
+
+            public array $todolist = array();
+
+            function save(Todolist $todolist): void {
+
+            }
+
+            function remove(int $number): bool {
+                
+            }
+
+            function findAll(): array {
+                return $this->todolist;
+            }
+
         }
     }
+
