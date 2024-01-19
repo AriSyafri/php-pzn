0ac216a2b74b14a6e368f3b50abab0ca467b2c06
AriSyafri <diamondThink15@gmail.com>
2024-01-19 22:10:33 +0700

    menambah service untuk implementasi interface dimulai  menampilkan data

--- 5_TodoListOOP/Service/TodolistService.php
+++ 5_TodoListOOP/Service/TodolistService.php
@@ -1,5 +1,7 @@
 <?php 
     namespace Service {
+
+        use Repository\TodolistRepository;
         interface TodolistService {
 
             function showTodolist(): void;
@@ -9,4 +11,32 @@
             function removeTodolist(int $number): void;
             
         }
+
+        class TodolistServiceImpl implements TodolistService {
+
+            private TodolistRepository $todolistRepository;
+
+            public function __construct (TodolistRepository $todolistRepository) {
+                $this->todolistRepository = $todolistRepository;
+            }
+
+            function showTodolist(): void {
+                echo "TODOLIST" . PHP_EOL;
+            
+                $todolist = $this->todolistRepository->findAll();
+
+                foreach ($todolist as $number => $value) {
+                    echo "$number . $value" . PHP_EOL; 
+                }
+            }
+
+            function addTodolist(string $todo): void {
+
+            }
+
+            function removeTodolist(int $number): void {
+
+            }
+        }
     }
+
