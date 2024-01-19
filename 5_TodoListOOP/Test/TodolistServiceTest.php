19b750d7d0bfa504816175d85509739eb6bb1346
AriSyafri <diamondThink15@gmail.com>
2024-01-19 22:29:42 +0700

    menambah pengujian tambah data

--- 5_TodoListOOP/Test/TodolistServiceTest.php
+++ 5_TodoListOOP/Test/TodolistServiceTest.php
@@ -4,19 +4,34 @@
     require __DIR__ . "/../Repository/TodolistRepository.php";
     require __DIR__ . "/../Service/TodolistService.php";
 
+    use Entity\Todolist;
     use Service\TodolistServiceImpl;
     use Repository\TodolistRepositoryImpl;
 
     function testShowTodolist(): void {
         $todolistRepository = new TodolistRepositoryImpl();
 
-        $todolistRepository->todolist[1] = "belajar php";
-        $todolistRepository->todolist[2] = "belajar php OOP";
-        $todolistRepository->todolist[3] = "belajar php MVC";
+        $todolistRepository->todolist[1] = new Todolist("belajar php");
+        $todolistRepository->todolist[2] = new Todolist("belajar php oop");
+        $todolistRepository->todolist[3] = new Todolist("belajar php mvc");
 
         $todolistService = new TodolistServiceImpl($todolistRepository);
 
         $todolistService->showTodolist();
     }
 
-    testShowTodolist();
+    function testAddTodolist(): void {
+        $todolistRepository = new TodolistRepositoryImpl();
+
+        $todolistService = new TodolistServiceImpl($todolistRepository);
+        $todolistService->addTodolist("belajar php8");
+        $todolistService->addTodolist("belajar php laravel");
+
+        $todolistService->showTodolist();
+    }
+
+
+
+    // testShowTodolist();
+
+    testAddTodolist();
