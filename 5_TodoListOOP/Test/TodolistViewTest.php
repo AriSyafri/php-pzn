92d95ced4b46402ac9f8c86b2e5e293288ab19a5
AriSyafri <diamondThink15@gmail.com>
2024-01-20 20:59:38 +0700

    menambah test view show todolist

---
+++ 5_TodoListOOP/Test/TodolistViewTest.php
@@ -0,0 +1,29 @@
+<?php
+
+    require __DIR__ . "/../Entity/Todolist.php";
+    require __DIR__ . "/../Repository/TodolistRepository.php";
+    require __DIR__ . "/../Service/TodolistService.php";
+    require __DIR__ . "/../View/TodolistView.php";
+    require __DIR__ . "/../Helper/InputHelper.php";
+
+    use \Entity\Todolist;
+    use \Repository\TodolistRepositoryImpl;
+    use \Service\TodolistServiceImpl;
+    use \View\TodolistView;
+
+    function testViewShowTodolist(): void {
+
+        $todolistRepository = new TodolistRepositoryImpl();
+        $todolistService = new TodolistServiceImpl($todolistRepository);
+        $todolistView = new TodolistView($todolistService);
+
+        $todolistService->addTodolist("belajar php8");
+        $todolistService->addTodolist("belajar php mvc");
+        $todolistService->addTodolist("belajar php laravel");
+        $todolistService->addTodolist("belajar php Database");
+
+        $todolistView->showTodolist();
+
+    } 
+
+    testViewShowTodolist();
