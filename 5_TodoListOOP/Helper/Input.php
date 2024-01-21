7fce8950cbe3dfefcfe2fcfac77fcd1538762212
AriSyafri <diamondThink15@gmail.com>
2024-01-21 12:19:41 +0700

    hapus program sebelumnya yang tidak dipakai

--- 5_TodoListOOP/Helper/Input.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-function input(string $info): string {
-    // menggunakan fungsi fgets untuk menginput data konsol
-    echo "$info : ";
-    $result = fgets(STDIN);
-    return trim($result);    
-}
--- 5_TodoListOOP/Test/TestInput.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-require_once __DIR__ . "/../Helper/Input.php";
-
-$name = input("name");
-echo "hello $name" . PHP_EOL;
-
-$name = input("name");
-echo "hello $name" . PHP_EOL;
