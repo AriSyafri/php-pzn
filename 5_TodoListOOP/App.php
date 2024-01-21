7d78b28c7af7e61b00d68ec581cb582cc706268f
AriSyafri <diamondThink15@gmail.com>
2024-01-21 12:19:19 +0700

    implementasi seluruh program

--- 5_TodoListOOP/App.php
+++ 5_TodoListOOP/App.php
@@ -1,18 +1,25 @@
 <?php
     // __DIR__ mengecek file direktori
 
-    require_once __DIR__ . "/Model/TodoList.php";
-    require_once __DIR__ . "/BusinessLogic/AddTodoList.php";
-    require_once __DIR__ . "/BusinessLogic/RemoveTodoList.php";
-    require_once __DIR__ . "/BusinessLogic/ShowTodoList.php";
-    require_once __DIR__ . "/View/ViewAddTodoList.php";
-    require_once __DIR__ . "/View/ViewRemoveTodoList.php";
-    require_once __DIR__ . "/View/ViewShowTodolist.php";
-    require_once __DIR__ . "/Helper/Input.php";
+    require_once __DIR__ . "/Entity/Todolist.php";
+    require_once __DIR__ . "/Repository/TodolistRepository.php";
+    require_once __DIR__ . "/Service/TodolistService.php";
+    require_once __DIR__ . "/View/TodolistView.php";
+    require_once __DIR__ . "/Helper/InputHelper.php";
     
+    use Repository\TodolistRepositoryImpl;
+    use Service\TodolistServiceImpl;
+    use View\TodolistView;
 
     echo "Aplikasi to do list" . PHP_EOL;
-    viewShowTodoList();
+
+    $todolistRepository = new TodolistRepositoryImpl();
+    $todolistService = new TodolistServiceImpl($todolistRepository);
+    $todolistView = new TodolistView($todolistService);
+
+    $todolistView->showTodolist();
+    
+    
     
 
 
